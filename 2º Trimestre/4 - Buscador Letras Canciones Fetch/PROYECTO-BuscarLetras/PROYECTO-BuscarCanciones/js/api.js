d6fed0f06eb7c1dcfc70a79b6421dacc36f4428e
patribasme99 <patri.basme.99@gmail.com>
2021-12-01 08:48:11 +0100

    Canciones Fetch Api JS - Terminado

---
+++ 2º Trimestre/4 - Buscador Letras Canciones Fetch/PROYECTO-BuscarLetras/PROYECTO-BuscarCanciones/js/api.js
@@ -0,0 +1,25 @@
+//Importamos la interfaz
+
+import * as UI from './interfaz.js';
+
+class API {
+
+    constructor(artista, cancion) {
+        this.artista = artista;
+        this.cancion = cancion;
+    }
+
+    consultarAPI() {
+        const url = `https://api.lyrics.ovh/v1/${this.artista}/${this.cancion}`;
+
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado => {
+                const { lyrics } = resultado;
+                UI.divResultado.textContent = lyrics;
+                UI.headingResultado.textContent = `${this.cancion} de ${this.artista}`;
+            });
+    }
+}
+
+export default API;
